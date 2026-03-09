854b41de4af3ec9cd15ab6a57cebe47c025009c4
lensing1006 <lensing1006@gmail.com>
2026-03-09 15:31:42 +0800

    - 提交项目

---
+++ buyer/public/config.js
@@ -0,0 +1,17 @@
+var BASE = {
+  /**
+   * @description api请求基础路径
+   */
+  API_DEV: {
+    common: "https://common-api.guiguishop.top",
+    buyer: "https://buyer-api.guiguishop.top",
+    seller: "https://store-api.guiguishop.top",
+    manager: "https://admin-api.guiguishop.top"
+  },
+  API_PROD: {
+    common: "https://common-api.guiguishop.top",
+    buyer: "https://buyer-api.guiguishop.top",
+    seller: "https://store-api.guiguishop.top",
+    manager: "https://admin-api.guiguishop.top"
+  },
+};
---
+++ seller/public/config.js
@@ -0,0 +1,29 @@
+var BASE = {
+  /**
+   * @description api请求基础路径
+   */
+  API_DEV: {
+    common: "https://common-api.guiguishop.top",
+    buyer: "https://buyer-api.guiguishop.top",
+    seller: "https://store-api.guiguishop.top",
+    manager: "https://admin-api.guiguishop.top",
+  },
+  API_PROD: {
+    common: "https://common-api.guiguishop.top",
+    buyer: "https://buyer-api.guiguishop.top",
+    seller: "https://store-api.guiguishop.top",
+    manager: "https://admin-api.guiguishop.top",
+  },
+  /**
+   * @description // 跳转买家端地址 pc端
+   */
+  PC_URL: "https://pc-b2b2c.guiguishop.top",
+  /**
+   * @description  // 跳转买家端地址 wap端
+   */
+  WAP_URL: "https://m-b2b2c.guiguishop.top",
+  /**
+   *  @description api请求基础路径前缀
+   */
+  PREFIX: "/store",
+};
