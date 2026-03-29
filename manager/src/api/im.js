7b5b63166eb565606d1ce84b11d1791c4a792078
lensing1006 <lensing1006@gmail.com>
2026-03-29 23:18:35 +0800

    fix(manager)：管理端打赏记录、打赏统计

--- manager/src/api/im.js
+++ manager/src/api/im.js
@@ -5,24 +5,42 @@ import {
   deleteRequest,
 } from "@/libs/axios";
 
-//   获取分页
+// 获取礼物分页
 export const getChatGiftList = (params) => {
   return getRequest("/im/chat/gift/list", params);
 };
 
+// 获取礼物详情
+export const getChatGiftData = (id) => {
+  return getRequest(`/im/chat/gift/get/${id}`);
+};
 
-// 删除
+// 删除礼物
 export const delChatGift = (id) => {
   return deleteRequest(`/im/chat/gift/delete/${id}`);
 };
 
-
-// 添加
+// 添加礼物
 export const addChatGift = (data) => {
   return postRequest("/im/chat/gift/create", data, { 'Content-Type': 'application/json' });
 };
 
-// 编辑
+// 编辑礼物
 export const editChatGift = (data) => {
   return putRequest(`/im/chat/gift/${data.id}`, data, { 'Content-Type': 'application/json' });
+};
+
+// 获取打赏记录分页
+export const getChatRewardList = (params) => {
+  return getRequest("/im/chat/reward/list", params);
+};
+
+// 删除打赏记录
+export const delChatReward = (id) => {
+  return deleteRequest(`/im/chat/reward/delete/${id}`);
+};
+
+// 获取打赏统计分页
+export const getMemberIncomeList = (params) => {
+  return getRequest("/member/income/list", params);
 };
--- manager/src/views/im/memberIncome.vue
+++ manager/src/views/im/memberIncome.vue
@@ -2,18 +2,21 @@
   <div class="search">
     <Card>
       <Row @keydown.enter.native="handleSearch">
-        <Form ref="searchForm" :model="searchForm" inline :label-width="70" class="search-form">
-          <Form-item label="礼物名称" prop="giftName">
-            <Input type="text" v-model="searchForm.giftName" placeholder="请输入礼物名称" clearable style="width: 240px" />
+        <Form ref="searchForm" :model="searchForm" inline :label-width="80" class="search-form">
+          <Form-item label="试穿员ID" prop="toMemberId">
+            <Input type="text" v-model="searchForm.memberId" placeholder="请输入试穿员ID" clearable style="width: 160px" />
+          </Form-item>
+          <Form-item label="试穿员名称" prop="toMemberName">
+            <Input type="text" v-model="searchForm.memberName" placeholder="请输入试穿员名称" clearable style="width: 160px" />
           </Form-item>
           <Button @click="handleSearch" class="search-btn" type="primary" icon="ios-search">搜索</Button>
         </Form>
       </Row>
     </Card>
     <Card>
-      <Row class="operation padding-row">
-        <Button @click="addChatGift" type="primary">添加礼物</Button>
-      </Row>
+<!--      <Row class="operation padding-row">-->
+<!--        <Button @click="addChatGift" type="primary">添加打赏记录</Button>-->
+<!--      </Row>-->
 
       <Table :loading="loading" :columns="columns" class="mt_10" :data="data" ref="table"></Table>
       <Row type="flex" justify="end" class="mt_10">
@@ -27,51 +30,52 @@
       <ossManage @callback="callbackSelected" :isComponent="true" :initialize="picModelFlag" ref="ossManage" />
     </Modal>
 
-    <!-- 添加/编辑礼物模态框 -->
-    <Modal v-model="modalFlag" :title="modalTitle">
-      <Form ref="modalForm" :model="modalData" :rules="rule" :label-width="100">
-        <FormItem label="名称" prop="giftName" style="width: 90%;">
-          <Input v-model="modalData.giftName" clearable />
-        </FormItem>
-        <FormItem label="图片" prop="giftImage" style="width: 90%">
-          <Avatar
-            style="height: 100px; width: 100px"
-            v-if="modalData.giftImage"
-            shape="square"
-            icon="ios-person"
-            size="default"
-            :src="modalData.giftImage"
-          />
-          <div>
-            <Button @click="handleCLickImg" type="primary"
-            >选择图片</Button
-            >
-          </div>
-        </FormItem>
-        <FormItem label="价格" prop="coinPrice" style="width: 90%;">
-          <Input v-model.number="modalData.coinPrice" clearable />
-        </FormItem>
-        <FormItem label="排序" prop="sort" style="width: 90%;">
-          <Input v-model.number="modalData.sort" clearable />
-        </FormItem>
-        <FormItem label="状态" prop="status">
-          <RadioGroup button-style="solid" v-model="modalData.status">
-            <Radio :label="1">启用</Radio>
-            <Radio :label="0">禁用</Radio>
-          </RadioGroup>
-        </FormItem>
-      </Form>
-      <div slot="footer">
-        <Button @click="modalFlag = false">取消</Button>
-        <Button type="primary" @click="submit">确定</Button>
-      </div>
-    </Modal>
+    <!-- 添加/编辑打赏记录模态框 -->
+<!--    <Modal v-model="modalFlag" :title="modalTitle">-->
+<!--      <Form ref="modalForm" :model="modalData" :rules="rule" :label-width="100">-->
+<!--        <FormItem label="名称" prop="giftName" style="width: 90%;">-->
+<!--          <Input v-model="modalData.giftName" clearable />-->
+<!--        </FormItem>-->
+<!--        <FormItem label="图片" prop="giftImage" style="width: 90%">-->
+<!--          <Avatar-->
+<!--            style="height: 100px; width: 100px"-->
+<!--            v-if="modalData.giftImage"-->
+<!--            shape="square"-->
+<!--            icon="ios-person"-->
+<!--            size="default"-->
+<!--            :src="modalData.giftImage"-->
+<!--          />-->
+<!--          <div>-->
+<!--            <Button @click="handleCLickImg" type="primary"-->
+<!--            >选择图片</Button-->
+<!--            >-->
+<!--          </div>-->
+<!--        </FormItem>-->
+<!--        <FormItem label="价格" prop="coinPrice" style="width: 90%;">-->
+<!--          <Input v-model.number="modalData.coinPrice" clearable />-->
+<!--        </FormItem>-->
+<!--        <FormItem label="排序" prop="sort" style="width: 90%;">-->
+<!--          <Input v-model.number="modalData.sort" clearable />-->
+<!--        </FormItem>-->
+<!--        <FormItem label="状态" prop="status">-->
+<!--          <RadioGroup button-style="solid" v-model="modalData.status">-->
+<!--            <Radio :label="1">启用</Radio>-->
+<!--            <Radio :label="0">禁用</Radio>-->
+<!--          </RadioGroup>-->
+<!--        </FormItem>-->
+<!--      </Form>-->
+<!--      <div slot="footer">-->
+<!--        <Button @click="modalFlag = false">取消</Button>-->
+<!--        <Button type="primary" @click="submit">确定</Button>-->
+<!--      </div>-->
+<!--    </Modal>-->
   </div>
 </template>
 
 <script>
 import * as API_ChatGift from "@/api/im.js";
 import ossManage from "@/views/sys/oss-manage/ossManage.vue";
+import {getMemberIncomeList} from "@/api/im.js";
 
 export default {
   name: "chatGift",
@@ -100,43 +104,34 @@ export default {
         pageSize: 20, // 页面大小
         order: "desc",
         id: "",
-        username: "",
-        mobile: "",
+        fromMemberId: "",
+        fromMemberName: "",
+        toMemberId: "",
+        toMemberName: "",
       },
-      rule: {
-        // 验证规则
-        giftName: [
-          { required: true, message: "请输入礼物名称" }
-        ],
-        coinPrice: [
-          { required: true, message: "请输入价格" }
-        ],
-        giftImage: [{ required: true, message: "请上传图片" }],
-      },
+      // rule: {
+      //   // 验证规则
+      //   giftName: [
+      //     { required: true, message: "请输入礼物名称" }
+      //   ],
+      //   coinPrice: [
+      //     { required: true, message: "请输入价格" }
+      //   ],
+      //   giftImage: [{ required: true, message: "请上传图片" }],
+      // },
       columns: [
         {
-          title: "礼物名称",
-          key: "giftName",
+          title: "试穿员ID",
+          key: "memberId",
         },
         {
-          title: "状态",
-          key: "status",
-          align: "center",
-          render: (h, params) => {
-            const status = params.row.status;
-            const text = status ? "启用" : "禁用";
-
-            return h("span", text);
-          }
-        },
-        {
-          title: "图片",
-          key: "giftImage",
+          title: "试穿员头像",
+          key: "storeLogo",
           align: "center",
           render: (h, params) => {
             return h("img", {
               attrs: {
-                src: params.row.giftImage || require('@/assets/emptyImg.png'),
+                src: params.row.storeLogo || require('@/assets/emptyImg.png'),
                 alt: ""
               },
               style: {
@@ -149,69 +144,69 @@ export default {
           }
         },
         {
-          title: "价格",
-          key: "coinPrice",
+          title: "试穿员",
+          key: "memberName",
         },
         {
-          title: "排序",
-          key: "sort",
+          title: "总收益",
+          key: "totalIncome",
         },
         {
-          title: "创建时间",
-          key: "createTime",
+          title: "今日收益",
+          key: "todayIncome",
         },
-        {
-          title: "操作",
-          key: "action",
-          align: "center",
-          fixed: "right",
-          render: (h, params) => {
-            return h("div", { class: "ops" }, [
-              h(
-                "a",
-                {
-                  style: {
-                    color: "#2d8cf0",
-                    cursor: "pointer",
-                    textDecoration: "none",
-                  },
-                  on: {
-                    click: () => {
-                      this.edit(params.row);
-                    },
-                  },
-                },
-                "修改"
-              ),
-              h(
-                "span",
-                {
-                  style: {
-                    margin: "0 8px",
-                    color: "#dcdee2",
-                  },
-                },
-                "|"
-              ),
-              h(
-                "a",
-                {
-                  style: {
-                    color: "#2d8cf0",
-                    cursor: "pointer",
-                    textDecoration: "none",
-                  },
-                  on: {
-                    click: () => {
-                      this.remove(params.row);
-                    },
-                  },
-                },
-                "删除"
-              ),
-            ]);
-          },
-        },
+        // {
+        //   title: "操作",
+        //   key: "action",
+        //   align: "center",
+        //   fixed: "right",
+        //   render: (h, params) => {
+        //     return h("div", { class: "ops" }, [
+        //       h(
+        //         "a",
+        //         {
+        //           style: {
+        //             color: "#2d8cf0",
+        //             cursor: "pointer",
+        //             textDecoration: "none",
+        //           },
+        //           on: {
+        //             click: () => {
+        //               this.edit(params.row);
+        //             },
+        //           },
+        //         },
+        //         "修改"
+        //       ),
+        //       h(
+        //         "span",
+        //         {
+        //           style: {
+        //             margin: "0 8px",
+        //             color: "#dcdee2",
+        //           },
+        //         },
+        //         "|"
+        //       ),
+        //       h(
+        //         "a",
+        //         {
+        //           style: {
+        //             color: "#2d8cf0",
+        //             cursor: "pointer",
+        //             textDecoration: "none",
+        //           },
+        //           on: {
+        //             click: () => {
+        //               this.remove(params.row);
+        //             },
+        //           },
+        //         },
+        //         "删除"
+        //       ),
+        //     ]);
+        //   },
+        // },
       ],
       data: [], // 表单数据
       total: 0, // 表单数据总数
@@ -230,16 +225,16 @@ export default {
     },
     edit(v) {
       this.modalFlag = true;
-      this.modalTitle = `修改礼物`;
+      this.modalTitle = `修改打赏记录`;
       this.getChatGiftInfo(v.id);
     },
     remove(v) {
       this.$Modal.confirm({
         title: "确认删除",
-        content: "您确认要删除该礼物?",
+        content: "您确认要删除该打赏记录?",
         loading: true,
         onOk: () => {
-          API_ChatGift.delChatGift(v.id).then((res) => {
+          API_ChatGift.delChatReward(v.id).then((res) => {
             this.$Modal.remove();
             this.$Message.success("删除成功");
             this.getData();
@@ -273,12 +268,12 @@ export default {
     },
     addChatGift() {
       this.modalFlag = true;
-      this.modalTitle = `添加礼物`;
+      this.modalTitle = `添加打赏记录`;
       this.resetForm();
       console.log(this.modalData);
     },
     /**
-     * 查询查看礼物详情
+     * 查询查看打赏记录详情
      */
     getChatGiftInfo(id) {
       API_ChatGift.getChatGiftData(id).then((res) => {
@@ -288,9 +283,9 @@ export default {
       });
     },
 
-    //查询礼物列表
+    //查询打赏记录列表
     getData() {
-      API_ChatGift.getChatGiftList(this.searchForm).then((res) => {
+      API_ChatGift.getMemberIncomeList(this.searchForm).then((res) => {
         if (res.result.records) {
           this.loading = false;
           this.data = res.result.records;
@@ -306,7 +301,7 @@ export default {
         this.addChatGiftSubmit();
       }
     },
-    //添加礼物提交
+    //添加打赏记录提交
     addChatGiftSubmit() {
       this.$refs.modalForm.validate((valid) => {
         if (valid) {
